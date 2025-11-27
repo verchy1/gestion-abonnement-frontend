3efdb4ec5d436dbe571d1ee86ae7518b1fddabb4
verchy1 <verchyfresneloba@gmail.com>
2025-11-27 14:14:29 +0100

    Update API_URL to point to the production backend

--- src/types.ts
+++ src/types.ts
@@ -45,4 +45,4 @@ export interface Stats {
   paiementsEnAttente: number;
 }
 
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://gestion-abonnement-backend.onrender.com/api';
